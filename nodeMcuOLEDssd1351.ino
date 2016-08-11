9e198f5098438de090bad6d747219eb18dbdf95b
Kunchala Anil <anilkunchalaece@gmail.com>
2016-08-11 19:09:59 +0530

    Adafruit ssd1351 Oled Display
    Using Arduino IDE

---
+++ nodeMcuOLEDssd1351.ino
@@ -0,0 +1,85 @@
+/*************************************************** 
+  This is a example sketch demonstrating graphic drawing
+  capabilities of the SSD1351 library for the 1.5" 
+  and 1.27" 16-bit Color OLEDs with SSD1351 driver chip
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/1431
+  ------> http://www.adafruit.com/products/1673
+ 
+  If you're using a 1.27" OLED, change SSD1351HEIGHT in Adafruit_SSD1351.h
+   to 96 instead of 128
+
+  These displays use SPI to communicate, 4 or 5 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+
+  The Adafruit GFX Graphics core library is also required
+  https://github.com/adafruit/Adafruit-GFX-Library
+  Be sure to install it!
+ ****************************************************/
+
+// You can use any (4 or) 5 pins 
+#define sclk D5
+#define mosi D7
+#define dc   D2
+#define cs   D8
+#define rst  D1
+
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+#include <SPI.h>
+
+// Option 1: use any pins but a little slower
+Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, mosi, sclk, rst);  
+
+// Option 2: must use the hardware SPI pins 
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, rst);
+
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0,0);
+  tft.setTextColor(color);
+  tft.print(text);
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("hello!");
+  tft.begin();
+
+  Serial.println("init");
+
+ 
+
+  tft.fillScreen(BLACK);
+  testdrawtext("Kunchala Anil From KSRMCE KADAPA...... Innovation Lab", WHITE);
+  delay(500);
+}
+
+void loop() {
+  
+//  tft.fillScreen(BLACK);
+//  testdrawtext("Kunchala Anil From KSRMCE", WHITE);
+//  delay(500);
+}
+
+
